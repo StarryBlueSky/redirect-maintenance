2119b8758bc786aae1dc3559585e59a1896f18e0
Nep <spica@starry.blue>
2022-01-31 20:02:53 +0900

    Publish

---
+++ index.js
@@ -0,0 +1,7 @@
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(_request) {
+  return Response.redirect("https://starry.blue/maintenance", 302);
+}
